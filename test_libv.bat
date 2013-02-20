c7ab89e766bd52ad1d577aa0d80879b01b09cf61
Michael Frechtling <michael.frechtling@sydney.edu.au>
2013-02-20 14:27:27 +1100

    Created batch file for testing in GHDL for windows
    Batch script runs same set of commands as original test_libv
    script, but first checks the GHDL version. Script has been
    tested using GHDL 0.25 and 0.29, known issue with simulation
    under 0.29 (WINDOWS) causes simulation to hang. Script will
    warn user if GHDL version is greater than 0.25 and ask if they
    wish to continue

---
+++ test_libv.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+setlocal
+
+set TESTEDVER=25
+for /f "tokens=2" %%g in ('ghdl -v ^| findstr /i "edition"') do (
+	set GHDLVER=%%g
+)
+set GHDLVER=%GHDLVER:"=%
+for /f "delims=. tokens=1-2" %%v in ("%GHDLVER%") do (
+	set MAJORVER=%%v
+	set MINORVER=%%w
+)
+set MAJORVER=%MAJORVER:"=%
+set MINORVER=%MINORVER:"=%
+if %MINORVER% GTR %TESTEDVER% (
+	@echo WARNING: CURRENT VERSION OF GHDL IS %MAJORVER%.%MINORVER% - test_libv.bat has only been tested using GHDL 0.25 due to known issues with simulations in later versions of GHDL for Windows
+	choice /M "Do you wish to continue" /c YN
+	if errorlevel 255 (
+	  echo Error
+	) else if errorlevel 2 (
+	  goto :no
+	) else if errorlevel 1 (
+	  goto :yes
+	) else if errorlevel 0 (
+	  echo Ctrl+C pressed.
+	)
+)
+:yes
+ghdl -a libv.vhd
+ghdl -e tb_libv
+ghdl -r tb_libv  --assert-level=error
+ECHO ------------------------------------------
+ECHO If you see can read this, things worked OK!
+:no
+endlocal
